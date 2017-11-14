b7a5d686838f393f1b57fc7471446cd793029b04
Rafael Martins <rafael.martins@cgibrasil.onmicrosoft.com>
2017-11-14 14:46:31 -0200

    Final do curso

---
+++ Financas/Financas/Controllers/MembershipCreatingException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Financas.Controllers
+{
+    [Serializable]
+    internal class MembershipCreatingException : Exception
+    {
+        public MembershipCreatingException()
+        {
+        }
+
+        public MembershipCreatingException(string message) : base(message)
+        {
+        }
+
+        public MembershipCreatingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MembershipCreatingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
